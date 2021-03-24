99b44b66da3d6388067f8fdf5409e1e8ca8043ce
Yanek K <yanek.dev@gmail.com>
2021-03-24 04:18:17 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import Header from "./components/Header";
+import "./default.scss";
+import Homepage from "./pages/Homepage";
+
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <Header />
+      <div className="main">
+        <Homepage />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Directory/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./styles.scss";
+import Shop35mm from "./../../assets/shop35mm.jpg";
+import ShopMediumFormat from "./../../assets/shopMediumFormat.jpeg";
+
+const Directory = (props) => {
+  return (
+    <div className="directory">
+      <div className="wrap" style={{ paddingLeft: "10px" }}>
+        <div
+          className="item"
+          style={{
+            backgroundImage: `url(${Shop35mm})`,
+            marginRight: "15px",
+          }}
+        >
+          <a>Shop 35mm Cameras</a>
+        </div>
+        <div
+          className="item"
+          style={{ backgroundImage: `url(${ShopMediumFormat})` }}
+        >
+          <a>Shop Medium Format</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Directory;
