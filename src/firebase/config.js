999aa769b0cf6d8c6ee683d0ba1b944e5d4a3072
Yanek-K <76711405+Yanek-K@users.noreply.github.com>
2021-03-24 08:55:58 -0400

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,8 +1,3 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyCC_xYr70t5mJDn1lG99XbGHWHMZGCNGfw",
-  authDomain: "premium-cameras.firebaseapp.com",
-  projectId: "premium-cameras",
-  storageBucket: "premium-cameras.appspot.com",
-  messagingSenderId: "1039441858488",
-  appId: "1:1039441858488:web:c51d61db2c849beab6094d",
+
 };
